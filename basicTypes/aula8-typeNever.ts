35f34836722f92b7af3717b90653858a0067de2b
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-07-23 10:48:45 -0300

    Delete basicTypes directory

--- basicTypes/aula8-typeNever.ts
+++
@@ -1,6 +0,0 @@
-// Never - significa que ele nunca vai retornar nada ou seja ela vai lançar um erro
-export function createError(): never {
-    throw new Error('Erro qualque');
-}
-
-createError();
