96b69408a76b5987e0717148a607ff358eb6426b
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-08-07 15:38:33 -0300

    Subindo Arquivos

---
+++ src/TypesAdvanceds/aula1-typeGuard.ts
@@ -0,0 +1,25 @@
+// Type Guard - Checar para refinar um tipo
+
+export function sum(a: unknown, b: unknown): string | number {
+    return typeof a === 'number' && typeof b === 'number' ? a + b : `${a}${b}` 
+}
+
+console.log(sum(3,3));
+console.log(sum('3','3'));
+
+type People = {type: 'people'; name: string};
+type Animal = {color: string};
+
+type PeopleOrAnimal = People | Animal;
+
+export class Aluno implements People {
+    type: "people" = 'people';
+    constructor(public name: string) {}
+}
+
+function mostrarName(obj: PeopleOrAnimal): void {
+    // if ('name' in obj) console.log(obj.name);
+    // if (obj instanceof Aluno) console.log(obj.name);
+}
+
+mostrarName(new Aluno('Moisés'));
