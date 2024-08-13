bd1a26017293b4e2a51a1366b5dc3cba47c3b7df
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-08-13 10:55:19 -0300

    Delete basicTypes directory

--- basicTypes/aula10-typeUnknown.ts
+++
@@ -1,9 +0,0 @@
-// Unknown - Ele é uma alternativa mais segura do tipo ANY
-let x: unknown;
-x = 10;
-x = 20;
-x = "Moisés";
-x = 30;
-const y = 20;
-
-if(typeof x === 'number') console.log(x + y);
--- basicTypes/aula11-unionTypes.ts
+++
@@ -1,8 +0,0 @@
-// Union Types - Significa que os paranetros ou variaveis podem ter mais de um tipo
-function sumAndConcat(n1: number | string, n2: number | string ): number | string {
-    if(typeof n1 === 'number' && typeof n2 === 'number') return n1 + n2;
-    return `${n1}${n2}`;
-}
-
-console.log(sumAndConcat(20,50));
-console.log(sumAndConcat("20","50"));
