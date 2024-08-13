adf9f2c4f405c7ce2d7e691ecb75bce1f80771c3
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-08-13 10:54:58 -0300

    Delete TypeGeneric directory

--- TypeGeneric/aula6-intersection.ts
+++
@@ -1,8 +0,0 @@
-export function unirObjetos<T1,T2>(obj1: T1, obj2: T2): T1 & T2 {
-    return {...obj1, ...obj2};
-}
-
-const obj1 = {ke1: 'Valor1'};
-const obj2 = {ke2: 'Valor2'};
-const uniao = unirObjetos(obj1,obj2)
-console.log(uniao);
